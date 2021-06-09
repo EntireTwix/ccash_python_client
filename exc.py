cb991cfd01ddc50643b9d681bee3a727aed0cba3
doggo <fearlessdoggo21@vivaldi.net>
2021-06-08 23:45:23 -0500

    Added completely useless comments in exception file

--- exc.py
+++ exc.py
@@ -1,10 +1,31 @@
 class UserNotExists(Exception):
-    def __init__(self, user):
+    '''
+    Exception for a user that does not exist within the server
+    '''
+
+    def __init__(self, user: str):
+        '''
+        Initializes a new instance of the UserNotExists exception
+
+        `user`: the username of a user not within the server
+        '''
+
         self.user = user
 
 
 class InvalidPassword(Exception):
-    def __init__(self, user, password):
+    '''
+    Exception for an incorrect password
+    '''
+
+    def __init__(self, user: str, pw: str):
+        '''
+        Initializes a new instance of the InvalidePassword exception
+
+        `user`: the username of the user  
+        `pw`: the invalid password
+        '''
+
         self.user = user
-        self.password = password
+        self.pw = pw
 
