1f646acb60b9972697555dded985f8b13c186b8f
doggo <fearlessdoggo21@vivaldi.net>
2021-06-18 16:45:29 -0500

    update setup.py to not error

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@
 ## python3 setup.py sdist
 ## twine upload dist/*
 
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    desc = file.read()
 
 setup(
     name                = "CCashPythonClient",
     packages            = ["CCashPythonClient"],
-    version             = "0.1.0",
+    version             = "0.1.1",
     license             = "MIT",
     description         = "A web-client to query CCash servers, providing ease of development.",
-    long_description    = open("README.md", "r").read(),
+    long_description    = desc, 
+    long_description_content_type = "text/markdown",
     author              = "FearlessDoggo21",
     author_email        = "fearlessdoggo21@vivaldi.net",
     url                 = "https://GitHub.com/FearlessDoggo21/CCashPythonClient",
